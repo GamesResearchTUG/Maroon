8499d3f6b9734f34fc1742cd8b849554c0c4e759
Michael Stefan Holly <michael.holly@tugraz.at>
2021-07-16 14:11:36 +0200

    Maroon-343 [Maroon VR] Gamified ZDF Version

---
+++ unity/Assets/Scripts/DiamondCollector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class DiamondCollector : MonoBehaviour
+{
+    private GameObject[] _diamonds;
+
+    private int _collectedDiamonds;
+
+    public int DiamondCount;
+
+
+    [SerializeField] private GameObject[] stars = new GameObject[3];
+
+    private void Start()
+    {
+        _diamonds = GameObject.FindGameObjectsWithTag("Diamond");
+    }
+
+    public void CollectDiamond()
+    {
+        _collectedDiamonds++;
+        UpdateStars();
+    }
+
+    private void UpdateStars()
+    {
+
+    }
+}
